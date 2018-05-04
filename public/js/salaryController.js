00d8fb2e39f4b11f908c00a4c2ed491b0abbd31b
nhamh <minhnha9z@gmail.com>
2018-05-04 20:02:16 +0700

    fix bug angular

--- public/js/salaryController.js
+++ public/js/salaryController.js
@@ -1,31 +1,13 @@
 angular.module('educationApp').controller('salaryController', function($scope, $http) {
-    $scope.init = function() {
-        $('select[name="month"]').attr('ng-init', 'monthSelected="' + new Date().getMonth() + '"');
-        $('select[name="year"]').attr('ng-init', 'yearSelected="' + eval(new Date().getYear() + 1900) + '"');
-        $data = {month: $scope.monthSelected, 
-                year: $scope.yearSelected};
-        $http({
-            url: './getSalary',
-            method: 'GET',
-            data: $data,
-            headers: {'Content-Type' : 'application/x-www-form-urlencoded'}
-        })
-        .then(function(response) {
-            $scope.salaryInfo = response.data;
-        }, function(response) {
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
-        });
-    }
-    $scope.init();
 
     $scope.update = function() {
-        $data = {month: $scope.monthSelected, 
-                year: $scope.yearSelected};
+        if (!($scope.monthSelected && $scope.yearSelected)) {
+            return;
+        }
         $http({
             url: './getSalary',
             method: 'GET',
-            data: $data,
+            params: {'month': $scope.monthSelected, 'year': $scope.yearSelected},
             headers: {'Content-Type' : 'application/x-www-form-urlencoded'}
         })
         .then(function(response) {
