025f51e605e302dce5255223814d352534488166
VTOcean <nobleeagle13@gmail.com>
2018-05-22 18:47:29 +0700

    update teacher manager into employee manager

--- resources/views/adminpage.blade.php
+++ resources/views/adminpage.blade.php
@@ -10,10 +10,9 @@
         <li><a data-toggle="tab" href="#menu4"><div class="arrow-left"></div><img src="./img/office.png">Quản lí trung tâm</a></li>
         <li><a data-toggle="tab" href="#menu5"><div class="arrow-left"></div><img src="./img/room.png">Quản lí phòng học</a></li>
         <li><a data-toggle="tab" href="#menu6"><div class="arrow-left"></div><img src="./img/employee.png">Quản lí nhân viên</a></li>
-        <li><a data-toggle="tab" href="#menu7"><div class="arrow-left"></div><img src="./img/teacher.png">Quản lí giáo viên</a></li>
-        <li><a data-toggle="tab" href="#menu8"><div class="arrow-left"></div><img src="./img/salary.png">Quản lí lương</a></li>
-        <li><a data-toggle="tab" href="#menu9"><div class="arrow-left"></div><img src="./img/promotion.png">Quản lí ưu đãi</a></li>
-        <li><a data-toggle="tab" href="#menu10"><div class="arrow-left"></div><img src="./img/statistic.png">Thống kê</a></li>
+        <li><a data-toggle="tab" href="#menu7"><div class="arrow-left"></div><img src="./img/salary.png">Quản lí lương</a></li>
+        <li><a data-toggle="tab" href="#menu8"><div class="arrow-left"></div><img src="./img/promotion.png">Quản lí ưu đãi</a></li>
+        <li><a data-toggle="tab" href="#menu9"><div class="arrow-left"></div><img src="./img/statistic.png">Thống kê</a></li>
     </ul>
 
     <div class="tab-content" ng-app="educationApp">
@@ -36,15 +35,12 @@
             @include('employee_manager')
         </div>
         <div id="menu7" class="tab-pane">
-            {{-- @include('teacher_manager') --}}
-        </div>
-        <div id="menu8" class="tab-pane">
             @include('salary_manager')
         </div>
-        <div id="menu9" class="tab-pane">
+        <div id="menu8" class="tab-pane">
             @include('promotion_manager')
         </div>
-        <div id="menu10" class="tab-pane">
+        <div id="menu9" class="tab-pane">
             @include('statistics_manager')
         </div>
     </div>
@@ -57,7 +53,6 @@
 <script src="js/CourseController.js"></script>
 <script src="js/OfficeController.js"></script>
 <script src="js/EmployeeController.js"></script>
-{{-- <script src="js/TeacherController.js"></script> --}}
 <script src="js/PromotionController.js"></script>
 <script src="js/StatisticController.js"></script>
 <script type="text/javascript">
