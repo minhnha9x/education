3066fd9b4facd4ad27e642db9c60f16b0db96d1d
nhamh <minhnha9z@gmail.com>
2018-05-26 01:40:20 +0700

    update new view

---
+++ app/Http/Controllers/ClassManageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use Auth;
+use Barryvdh\Debugbar\Facade as Debugbar;
+
+class ClassManageController extends Controller
+{
+    public function deleteClass(Request $r) {
+        $class = DB::table('class')
+        ->where('id', $r->id)
+        ->delete();
+        return back()->withInput();
+    }
+}
